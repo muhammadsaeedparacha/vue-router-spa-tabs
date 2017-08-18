c8a288a3ee74c4503fd1a5fbb102e9ed86ff61a8
Passionatedreamer <passionatedreamer19@gmail.com>
2017-08-18 09:40:49 +0500

    First working

--- src/index.js
+++ src/index.js
@@ -4,35 +4,28 @@ exports.tabs = function (store, router, options) {
 
   store.registerModule(moduleName, {
     namespaced: true,
-    state: [],
+    state: {},
     mutations: {
       'tabCreate': function (state, to) {
         state[to.meta.tab] = to.path
       },
       'tabDelete' : function (state, tabName) {
-        // foreach (state as tab => path ) {
-        //   if (tab == tabName) {
-        //     delete state[tab]
-        //     // state.$remove(tab)
-        //   }
-        // }
+        delete state[tab]
       }
     },
     actions: {
       'routeChanged': function ({state,commit}, to) {
-        if(!state[to.meta.tab]){
-          commit('tabCreate', to)
-        }
+        commit('tabCreate', to)
       },
       'tabDelete': function ({commit}, tabName) {
         commit('tabDelete', tabName)
       }
     }
   })
-
   router.afterEach(function (to, from) {
     const tab = to.meta.tab || false
-    if (tab)
+    if (tab){
       store.dispatch(moduleName + '/routeChanged', to)
+    }
   })
 }
